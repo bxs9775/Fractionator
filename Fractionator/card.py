5dba8c7d8d50271002710001115990e0507aac47
Brian Sandon <bsandon@localhost.localdomain>
2017-04-13 17:45:26 -0400

    Mouse event implementation v1.0 * This implementation makes a few assumptions: 	1- The doMouseDown, doMouseMove, and doMouseUp functions are being called by another class, that is managing the event handling 	2- The other class is passing a mouse object that contains the fields x and y 	3- The other class has already checked if the mouse coordinates are in the card's rectangle using pointOnCard(point), before calling the card's doMouseDown method, and the calling class has already checked if the card is being dragged before calling doMouseMove or doMouseUp

--- Fractionator/card.py
+++ Fractionator/card.py
@@ -21,17 +21,30 @@ class card(object):
 	#	image - an image representation of the card
 	def __init__(self,pos,fraction,image):
 		self.position = pos
+		self.width = 10		#subject to change
+		self.height = 50	#subject to change
+		
 		self.fraction = fraction
 		self.image = image
-	#Event handler for when the mouse selects the card
-	def onMouseDown(mouse):
-		#implementation needed
-	#Event handler for when the mouse drags the card
-	def onMouseMove(mouse):
-		#implementation needed
-	#Event handler for when the card is released
-	def onMouseUp(mouse):
-		#implementation needed
+		
+		self.beingDragged = false #I need a better name for this
 	#Draws the card to the screen
 	def draw(canvas):
 		#implementation needed
+
+	## MOUSE EVENTS
+	#Event handler for when the mouse selects the card
+	def onMouseDown(mouse):
+		self.beingDragged = true
+	#Event handler for when the mouse drags the card
+	def onMouseMove(mouse):
+		self.position = mouse
+	#Event handler for when the card is released
+	def onMouseUp(mouse):
+		self.beingDragged = true
+	
+	## HELPER FUNCTIONS
+	#Checks if a point is within the card's rectangle
+	def pointOnCard(point):
+		pos = self.position
+		return ((point.x >= pos.x && point.x <= (pos.x + self.width)) &&(point.y >= pos.y && point.y <= (pos.y + self.height))
