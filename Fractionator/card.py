a22df6e23e0fbed74c99cce63e2db7b7e0b37d9d
Brian Sandon <bsandon@localhost.localdomain>
2017-04-13 18:13:15 -0400

    Updated comments in card.py and tried compiling the code. * pointOnCard(point) currently is producing a syntax error I can't track down.

--- Fractionator/card.py
+++ Fractionator/card.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 #[File level license goes here]
 
 #I don't know which imports are relevent
@@ -31,20 +32,36 @@ class card(object):
 	#Draws the card to the screen
 	def draw(canvas):
 		#implementation needed
+		print "Drawing card"
 
 	## MOUSE EVENTS
 	#Event handler for when the mouse selects the card
+	#Params:
+	#	mouse - the object storing the mouse location - must have x 
+	#	and y fields
 	def onMouseDown(mouse):
 		self.beingDragged = true
+		print "Mouse down"
 	#Event handler for when the mouse drags the card
+	#Params:
+	#	mouse - the object storing the mouse location - must have x 
+	#	and y fields
 	def onMouseMove(mouse):
 		self.position = mouse
+		print "Mouse move"
 	#Event handler for when the card is released
+	#Params:
+	#	mouse - the object storing the mouse location - must have x 
+	#	and y fields
 	def onMouseUp(mouse):
 		self.beingDragged = true
+		print "Mouse up"
 	
 	## HELPER FUNCTIONS
 	#Checks if a point is within the card's rectangle
+	#Params:
+	#	point - the object storing the point to be checked - must have x 
+	#	and y fields
 	def pointOnCard(point):
 		pos = self.position
-		return ((point.x >= pos.x && point.x <= (pos.x + self.width)) &&(point.y >= pos.y && point.y <= (pos.y + self.height))
+		return ((point.x >= pos.x && point.x <= (pos.x + self.width)) &&(point.y >= pos.y && point.y <= (pos.y + self.height)))
